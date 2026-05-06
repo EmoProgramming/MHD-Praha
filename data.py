1dfcec47800c08dbf2540c9d0511d8a83d347abe
Emanuel Gransky <gransky.emanuel@gmail.com>
2026-05-06 18:32:58 +0200

    Update notes and have routes with its stops, lon and lat

--- data.py
+++ data.py
@@ -2,10 +2,17 @@ import re
 
 ROUTE = "stops/route_stops.txt"
 STOPS = "stops/stops.txt"
-
-INDEX_OF_STOP_ID = 2
+NUMBER_OF_TRAMS = 26
 
 stops = []
+routes = {"L"+str(i):[] for i in range(1, NUMBER_OF_TRAMS+1)}
+
+
+def get_route(number_of_route):
+    return routes["L"+str(number_of_route)]
+
+def print_route(number_of_route):
+    print("L"+str(number_of_route), routes["L"+str(number_of_route)])
 
 def find_stop(stop_id, stops):
     for i in range(len(stops)):
@@ -28,7 +35,6 @@ with open(ROUTE, "r") as route_file, open(STOPS, "r") as stops_file:
             lon = parts_of_line[3]
             stops.append([stop_id, stop_name, lan, lon])
 
-    pocitadlo = 0
 
     for line in route_file:
 
@@ -36,19 +42,17 @@ with open(ROUTE, "r") as route_file, open(STOPS, "r") as stops_file:
         if not re.match("^L([1-9]|1[0-9]|2[0-6]),", line):
             continue
 
-        pocitadlo += 1
-
         parts_of_line = line.split(",")
-        stop_id = parts_of_line[INDEX_OF_STOP_ID]
+        stop_id = parts_of_line[2]
+        route_id = parts_of_line[0]
 
         stop_info = find_stop(stop_id, stops)
 
+
         if stop_info == -1:
             print("not found")
-
-
-
-    print(pocitadlo)
+        else:
+            routes[route_id].append(stop_info)
 
 
 
